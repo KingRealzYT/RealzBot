069f5fbba7f53324e0baa330e15de6f11c801ff6
KingRealzYT <jonathansanie@gmail.com>
2022-06-21 01:28:42 -0500

    Changed the Join / Leave System to Embed

--- src/main/java/dev/realz/realzbot/listeners/EventListener.java
+++ src/main/java/dev/realz/realzbot/listeners/EventListener.java
@@ -1,12 +1,16 @@
 package dev.realz.realzbot.listeners;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.Instant;
 
 public class EventListener extends ListenerAdapter {
 
@@ -22,15 +26,23 @@ public class EventListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-        String user = event.getUser().getAsMention();
+        EmbedBuilder eb = new EmbedBuilder();
+        String user = event.getUser().getAsTag();
         String message = "Welcome to the server, " + user + "!";
-        event.getGuild().getSystemChannel().sendMessage(message).queue();
+        eb.setTitle(message);
+        eb.setTimestamp(Instant.now());
+        eb.setColor(0xd01212);
+        event.getGuild().getSystemChannel().sendMessageEmbeds(eb.build()).queue();
     }
 
     @Override
     public void onGuildMemberRemove(@NotNull GuildMemberRemoveEvent event) {
+        EmbedBuilder eb = new EmbedBuilder();
         String user = event.getUser().getAsTag();
         String message = "Awh Goodbye, " + user + " :(";
-        event.getGuild().getSystemChannel().sendMessage(message).queue();
+        eb.setTitle(message);
+        eb.setTimestamp(Instant.now());
+        eb.setColor(0xd01212);
+        event.getGuild().getSystemChannel().sendMessageEmbeds(eb.build()).queue();
     }
 }
